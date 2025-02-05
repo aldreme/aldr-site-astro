5b6768fe5d724426724bca3fc70857ac6673613d
ALDR EME <70865785+aldreme@users.noreply.github.com>
2025-02-05 01:30:30 -0500

    fix: gh-pages issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://aldreme.github.io',
+  base: 'aldr-site-astro',
   integrations: [
     react(),
     tailwind({
