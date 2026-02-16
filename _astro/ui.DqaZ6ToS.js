6401a4599dc219e0f6d3aac4281baaebedc813bc
aldreme <aldreme@users.noreply.github.com>
2026-02-16 08:38:17 +0000

    deploy: b085082d2aca6978fd8de932ab9f65130c206b54

--- _astro/ui.DqaZ6ToS.js
+++
@@ -1,1 +0,0 @@
-import{z as a,e}from"./zh_cn.HPmE8m0R.js";const c={en:"English",zh:"简体中文"},u="en",s={en:e,zh:a};function i(t){return function(n){return s[t][n]||s[u][n]}}export{u as d,c as l,i as u};
